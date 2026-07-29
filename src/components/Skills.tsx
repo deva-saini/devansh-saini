58a5b5b6252935c47d4727bf765e5918a81ef145
Ashwani Saini <ashwanisaini371@gmail.com>
2026-07-29 18:27:14 +0530

    remove unused import

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faBuysellads, faProductHunt, faDailymotion, } from '@fortawesome/free-brands-svg-icons';
+import { faBuysellads, faProductHunt, faDailymotion, } from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 import { faBoltLightning, faScissors, faVideo } from "@fortawesome/free-solid-svg-icons";
